b5ae051d0f9ffd555f9e76f632db925865d49e1c
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-06-17 19:12:25 +0200

    Add blog list route to the blog controller

--- webapp/src/Controller/BlogController.php
+++ webapp/src/Controller/BlogController.php
@@ -8,6 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/public/blog')]
 class BlogController extends BaseController
 {
+    #[Route(path: '', name: 'public_blog_list')]
+    public function blogListAction(): Response {
+        return $this->render('public/blog_list.html.twig');
+    }
+
     #[Route(path: '/{id}', name: 'public_blog_post')]
     public function blogPostAction(int $id): Response {
         return $this->render('public/blog_post.html.twig');
