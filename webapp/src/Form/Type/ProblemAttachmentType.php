600c14c29ac485a1ef66ef2a90856577c0ebc403
Lubos Zapotocny <zapotocnylubos@gmail.com>
2023-09-18 14:46:14 +0200

    Add possibility to set attachment type for jury members

--- webapp/src/Form/Type/ProblemAttachmentType.php
+++ webapp/src/Form/Type/ProblemAttachmentType.php
@@ -2,6 +2,9 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Language;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,6 +15,15 @@ class ProblemAttachmentType extends AbstractType
     {
         $builder->add('content', BootstrapFileType::class, [
             'required' => true,
+        ]);
+        $builder->add('language', EntityType::class, [
+            'class' => Language::class,
+            'required' => false,
+            'query_builder' => fn(EntityRepository $er) => $er
+                ->createQueryBuilder('l')
+                ->andWhere('l.allowSubmit = 1'),
+            'choice_label' => 'name',
+            'placeholder' => 'Select a language (optional)',
         ]);
         $builder->add('add', SubmitType::class, [
             'attr' => [
